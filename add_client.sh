f90c979b019cfc28bea0b2fa87cbabe6103d79c2
Maksim Nechaev <mvnechaev@avito.ru>
2022-04-06 23:16:35 +0300

    Show qr code for new client

--- add_client.sh
+++ add_client.sh
@@ -1,5 +1,11 @@
+echo Enter client name:
+read CLIENT_NAME
+
 source venv/bin/activate
-python add_client.py
+echo
+echo $CLIENT_NAME | python add_client.py
+echo
+qrencode -t ansiutf8 -r configs/$CLIENT_NAME.conf
 
 echo restarting wireguard...
 systemctl restart wg-quick@wg0.service
